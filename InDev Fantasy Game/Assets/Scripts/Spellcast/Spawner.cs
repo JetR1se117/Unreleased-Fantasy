55577317c057da99f85396f9d89e7b72b711bc35
Jet <jamesdanielcamber@gmail.com>
2021-03-01 02:49:30 +0000

    Fixed FireBlast Spell
    Fires in the direction you aim. Currently low speed, can be changed later. only shoots straight ahead of the player, not up/down. Again, this can be added down the line.

---
+++ InDev Fantasy Game/Assets/Scripts/Spellcast/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Spawner : MonoBehaviour
+{
+    public float projectileSpeed;
+
+    public float projectileLife = 3f;
+
+    private Rigidbody projectileRB;
+
+    void Awake()
+    {
+        projectileRB = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        //projectileRB.velocity = Vector3.forward * projectileSpeed;
+        Destroy(gameObject, projectileLife);
+    }
+
+/*
+ :::::OLD SCRIPT:::::
+        GameObject fireblast = Instantiate(projectile, transform) as GameObject;
+            Rigidbody rb = fireblast.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * projectileSpeed);    
+ * 
+ */
+}
