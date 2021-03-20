6ec17de92a5e3dfaa9c8afa116fff5602c4371a3
Dmitri <Dmitri.prihodko@tptlive.ee>
2021-03-20 15:39:24 +0000

    Kill streak and magic fix
    Now bullets flight straight and i made kill streak counter

---
+++ Assets/KillStreak.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillStreak : MonoBehaviour
+{
+    public int  killStreak = 0;
+    
+    public void TagetHitted(GameObject GO, bool killStreakCounted)
+    {
+        if (GO != null)
+        {
+            EnemyAI target = GO.GetComponent<EnemyAI>();
+            if (target.health <= 0)
+            {
+                killStreak++;
+                Debug.Log("kill steak: " + killStreak);
+            }
+        }
+    }
+}
